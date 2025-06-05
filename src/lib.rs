3297248f35b636cfd9d0616b0bd7dbcc95383563
blorbb <88137137+blorbb@users.noreply.github.com>
2025-06-05 20:15:31 +1000

    add docs for traits

--- src/lib.rs
+++ src/lib.rs
@@ -15,8 +15,9 @@
 //! ## Shared References
 //!
 //! The [`Share`] trait is an alias for [`Clone`].
-//! It should be used for clones that semantically represent some kind
-//! of reference counting or generally refer to the same object.
+//! It should be implemented on types that point to the same underlying
+//! data when cloned (like [`Rc`]/[`Arc`]), or more generally share
+//! some kind of state when cloned (like [`mpsc`] channel senders).
 //!
 //! You may want to enable the `clippy::clone_on_ref_ptr` lint for
 //! a warning when you clone on an [`Rc`]/[`Arc`].
@@ -97,9 +98,9 @@
 //! assert_eq!(*mutex.lock().or_poisoned(), 12);
 //! ```
 //!
-//! [`Clone`]: std::clone::Clone
 //! [`Rc`]: std::rc::Rc
 //! [`Arc`]: std::sync::Arc
+//! [`mpsc`]: std::sync::mpsc
 
 #![no_std]
 
@@ -108,18 +109,91 @@ extern crate std;
 
 mod impls;
 
+/// A cheap [`Clone`] with shared reference semantics.
+///
+/// This is implemented on types that point to the same underlying
+/// data when cloned (like [`Rc`]/[`Arc`]), or more generally share
+/// some kind of state when cloned (like [`mpsc`] channel senders).
+///
+/// You may want to enable the `clippy::clone_on_ref_ptr` lint for
+/// a warning when you clone on an [`Rc`]/[`Arc`].
+///
+/// ```
+/// use std::sync::{Arc, mpsc};
+/// use std::thread;
+/// use semtraits::Share;
+///
+/// let foo = Arc::new(vec![1, 2, 3]);
+/// // The two lines below are equivalent.
+/// let bar = foo.share();
+/// let baz = Arc::share(&foo);
+/// assert!(Arc::ptr_eq(&bar, &baz));
+///
+/// // An mpsc sender can also 'share' it's sender.
+/// let (tx, rx) = mpsc::channel();
+/// let tx1 = tx.share();
+/// thread::spawn(move || {
+///     tx1.send(10).unwrap();
+/// });
+/// thread::spawn(move || {
+///     tx.send(10).unwrap();
+/// });
+/// assert_eq!(rx.recv().unwrap(), 10);
+/// assert_eq!(rx.recv().unwrap(), 10);
+/// ```
+///
+/// [`Rc`]: std::rc::Rc
+/// [`Arc`]: std::sync::Arc
+/// [`mpsc`]: std::sync::mpsc
 pub trait Share: Clone {
     fn share(&self) -> Self {
         self.clone()
     }
 }
 
+/// Panic when a channel is closed but one side tries to send/receive.
+///
+/// This should be implemented on channels where an error is returned
+/// by `send`/`recv` methods *if and only if* it is impossible for
+/// the channel to send/receive a value again (i.e. the channel is
+/// disconnected/closed).
+///
+/// ```
+/// use std::sync::mpsc;
+/// use semtraits::OrClosed;
+///
+/// let (tx, rx) = mpsc::channel();
+/// // same as .expect(...)
+/// tx.send(10).or_closed();
+/// assert_eq!(rx.recv().or_closed(), 10);
+/// ```
+///
+/// This trait is implemented on the output of `tx.send()` and
+/// `rx.recv()` methods, which are generally [`Result`] types with
+/// a channel-specific error type.
 pub trait OrClosed {
     type Value;
 
     fn or_closed(self) -> Self::Value;
 }
 
+/// Gets the value out of a lock, panic if the lock has been poisoned.
+///
+/// ```
+/// use std::sync::Mutex;
+/// use semtraits::OrPoisoned;
+///
+/// let mutex = Mutex::new(1);
+///
+/// {
+///     let mut data = mutex.lock().or_poisoned();
+///     *data = 2;
+/// }
+///
+/// *mutex.lock().or_poisoned() += 10;
+///
+/// assert_eq!(*mutex.lock().or_poisoned(), 12);
+/// ```
 pub trait OrPoisoned {
     type Value;
 
